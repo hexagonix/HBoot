601f5e03cd2094c8d931e4a1621c16f98330eb0a
hexagonix <felipemiguel_nery@hotmail.com>
2021-12-24 23:16:43 -0300

    Mudança na logo de cabeçalho
    Mudança da logo no cabeçalho

--- Dev/BIOSx86.asm
+++ Dev/BIOSx86.asm
@@ -1,20 +1,25 @@
 ;;************************************************************************************
 ;;
 ;;    
-;;                        Carregador de Inicialização HBoot
-;;        
-;;                             Hexagon® Boot - HBoot
+;; ┌┐ ┌┐                                 Sistema Operacional Hexagonix®
+;; ││ ││
+;; │└─┘├──┬┐┌┬──┬──┬──┬─┐┌┬┐┌┐    Copyright © 2016-2022 Felipe Miguel Nery Lunkes
+;; │┌─┐││─┼┼┼┤┌┐│┌┐│┌┐│┌┐┼┼┼┼┘          Todos os direitos reservados
+;; ││ │││─┼┼┼┤┌┐│└┘│└┘││││├┼┼┐
+;; └┘ └┴──┴┘└┴┘└┴─┐├──┴┘└┴┴┘└┘
+;;              ┌─┘│          
+;;              └──┘          
+;;
+;;
+;;************************************************************************************
+;;    
+;;                                   Hexagon® Boot
+;;
+;;                   Carregador de Inicialização do Kernel Hexagon®
 ;;           
 ;;                 Copyright © 2020-2022 Felipe Miguel Nery Lunkes
 ;;                         Todos os direitos reservados
 ;;                                  
-;;************************************************************************************
-;;
-;;                                   Hexagon® Boot
-;;
-;;                   Carregador de Inicialização do Kernel Hexagon®
-;;
-;;
 ;;************************************************************************************
 
 ;; Aqui iremos utilizar os serviços do BIOS para provocar um atraso
--- Lib/modulos.asm
+++ Lib/modulos.asm
@@ -1,20 +1,25 @@
 ;;************************************************************************************
 ;;
 ;;    
-;;                        Carregador de Inicialização HBoot
-;;        
-;;                             Hexagon® Boot - HBoot
-;;           
-;;                Copyright © 2020-2022 Felipe Miguel Nery Lunkes
-;;                         Todos os direitos reservados
-;;                                  
-;;************************************************************************************
+;; ┌┐ ┌┐                                 Sistema Operacional Hexagonix®
+;; ││ ││
+;; │└─┘├──┬┐┌┬──┬──┬──┬─┐┌┬┐┌┐    Copyright © 2016-2022 Felipe Miguel Nery Lunkes
+;; │┌─┐││─┼┼┼┤┌┐│┌┐│┌┐│┌┐┼┼┼┼┘          Todos os direitos reservados
+;; ││ │││─┼┼┼┤┌┐│└┘│└┘││││├┼┼┐
+;; └┘ └┴──┴┘└┴┘└┴─┐├──┴┘└┴┴┘└┘
+;;              ┌─┘│          
+;;              └──┘          
 ;;
+;;
+;;************************************************************************************
+;;    
 ;;                                   Hexagon® Boot
 ;;
 ;;                   Carregador de Inicialização do Kernel Hexagon®
-;;
-;;
+;;           
+;;                 Copyright © 2020-2022 Felipe Miguel Nery Lunkes
+;;                         Todos os direitos reservados
+;;                                  
 ;;************************************************************************************
 
 include "modHBoot.asm"
